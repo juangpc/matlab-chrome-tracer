4c01b491511fac1dcfe538b4e30d0558a708a697
Juan GPC <juangpc@gmail.com>
2020-10-06 20:10:15 -0500

    update example actions script

--- actions.m
+++ actions.m
@@ -1,27 +1,32 @@
 function actions
+t__=Tracer(dbstack);
 
-t__=Tracer(dbstack); %#ok<NASGU>
-
+disp("starting actions")
 pause(2.5)
 
 action1
 
 action2
 
+disp("ending actions")
 end
+
 
 function action1
-t__=Tracer(dbstack); %#ok<NASGU>
+t__=Tracer(dbstack);
 
+disp("starting action1")
 pause(1.5)
-
+disp("ending action1")
 end
 
+
 function action2
-t__=Tracer(dbstack); %#ok<NASGU>
+t__=Tracer(dbstack);
 
+disp("starting action2")
 pause(2)
-
+disp("ending action2")
 end
 
 
