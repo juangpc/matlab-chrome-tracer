63583a4a8878bd4a6b1b50c20f15df3aa91f5ac2
Juan GPC <juangpc@gmail.com>
2020-10-09 16:00:30 -0500

    update actions example script

--- actions.m
+++ actions.m
@@ -1,8 +1,9 @@
 function actions
 t__=Tracer(dbstack);
 disp("starting actions")
-pause(2.5)
+c = cov(magic(5e3));
 action1
+pause(1)
 action2
 disp("ending actions")
 end
@@ -10,15 +11,22 @@ end
 function action1
 t__=Tracer(dbstack);
 disp("starting action1")
-pause(1.5)
+pause(2)
+c = cov(magic(7e3));
 disp("ending action1")
 end
 
 function action2
 t__=Tracer(dbstack);
 disp("starting action2")
-pause(2)
+c = cov(magic(1e4));
 disp("ending action2")
 action1
+pause(1.5)
 end
 
+% Tracer.trackMemory(true);
+% Tracer.enable
+% actions
+% Tracer.trackMemory(false);
+% Tracer.disable
